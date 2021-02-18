8edbf4e23049dfd9a89705453923163c8f05460b
Ong Ka Yi <kayiongky@gmail.com>
2021-02-18 22:00:52 +0800

    Insert student and events into firebase AND retrieve

--- src/Components/MyTimetable.js
+++ src/Components/MyTimetable.js
@@ -1,6 +1,4 @@
-import { React, Fragment, useState } from "react";
-
-import Body from "./Body";
+import { React, Fragment, useState, useEffect } from "react";
 
 import firebase from "firebase";
 import firebaseConfig from "../Firebase/firebaseConfig";
@@ -18,12 +16,111 @@ if (!firebase.apps.length) {
 }
 
 function MyTimetable(props) {
-  const [userList, setUserList] = useState({});
+  // ============================= STUDENTS =============================
+  const [totalStudentCount, setTotalStudentCount] = useState(0);
+  const [studentId, setStudentId] = useState(localStorage.getItem("studentId"));
+  const [student, setStudent] = useState([]);
+  const [studentEvents, setStudentEvents] = useState([
+    { endtime: "", eventname: "", eventtype: "", starttime: "" },
+  ]);
+  // 1. IF studentID NOT in browser memory
+  // >> store studentID to browser memory (local storage) AND database
 
-  let getUsers = () => {
-    var usersRef = database.ref("users/");
-    usersRef.once("value").then((snapshot) => {
-      snapshot.val() ? setUserList(snapshot.val()) : console.log("missing");
+  // 2. IF there is studentID in browser memory
+  // (a) no timetable
+  // >> can means no retrieval of data because he havent import etc
+
+  // (b) have timetable in the database
+  // >> retrieval JUST THAT student record based on this studentID)
+
+  // https://yourapp.firebaseio.com/posts/[the post key]/comments?shallow=true.
+  let getTotalStudents = () => {
+    //Retrieve this to generate new studentID by +1
+    var studentsRef = database.ref("Students/");
+    studentsRef.once("value").then((snapshot) => {
+      setTotalStudentCount(snapshot.numChildren());
+    });
+    console.log("***getTotalStudents():  " + totalStudentCount);
+  };
+
+  let getStudentEvents = (studentId) => {
+    var studentsRef = database.ref(`Students/${studentId}/events`);
+    studentsRef.once("value").then((snapshot) => {
+      setStudentEvents(snapshot.val());
+    });
+    console.log("***getStudentEvents():  " + JSON.stringify(studentEvents));
+  };
+
+  useEffect(() => {
+    // setStudentId(0); //foor testing purpose set first
+    // localStorage.setItem("studentId", 0); //foor testing purpose set first
+
+    getTotalStudents();
+    if (localStorage.getItem("studentId") == null) {
+      setStudentId(totalStudentCount + 1);
+      localStorage.setItem("studentId", studentId);
+      console.log(
+        "(1)localStorage.getItem(studentId): " +
+          localStorage.getItem("studentId")
+      );
+    } else {
+      setStudentId(localStorage.getItem("studentId"));
+      localStorage.setItem("studentId", studentId);
+      console.log(
+        "(2)localStorage.getItem(studentId): " +
+          localStorage.getItem("studentId")
+      );
+      getStudentEvents(localStorage.getItem("studentId"));
+      localStorage.setItem("studentEvents", studentEvents);
+    }
+
+    if (!Array.isArray(studentEvents) || !studentEvents.length) {
+      for (var i = 0; i < studentEvents.length; i++) {
+        console.log("***eventname:  " + studentEvents[i].eventname);
+        console.log("***eventtype:  " + studentEvents[i].eventtype);
+        console.log("***endtime:  " + studentEvents[i].endtime);
+        console.log("***starttime:  " + studentEvents[i].starttime);
+      }
+    }
+    // let eventsList = {};
+    // for (var key in studentEvents) {
+    //   if (studentEvents.hasOwnProperty(key)) {
+    //     console.log(key + " -> " + studentEvents[key]);
+    //     console.log(typeof studentEvents);
+    //     console.log("test!!!" + key[0]);
+    //   }
+    // }
+  }, []);
+
+  let addEvent = () => {
+    console.log("Event inserted into db!");
+    //this event data to be taken from Panna ***
+    var newEvent = {
+      endtime: "1900",
+      eventname: "IS4261 meeting",
+      eventtype: "school",
+      starttime: "1700",
+    };
+    // var studentsRef = database.ref(`Students/${studentId}/events/${studentEvents.length}`).push(newEvent);
+    var studentsRef = database.ref(`Students/${studentId}/events`);
+    studentsRef.child(studentEvents.length).set(newEvent);
+  };
+
+  let addStudent = () => {
+    console.log("Student inserted into db!");
+    let studentName = "Tom Lee";
+    var studentsRef = database.ref(`Students/`);
+    studentsRef
+      .child(totalStudentCount + 1)
+      .child("name")
+      .set(studentName);
+  };
+
+  const [studentsList, setStudentList] = useState({});
+  let getStudents = () => {
+    var studentsRef = database.ref("Students/");
+    studentsRef.once("value").then((snapshot) => {
+      snapshot.val() ? setStudentList(snapshot.val()) : console.log("missing");
     });
   };
 
@@ -38,34 +135,65 @@ function MyTimetable(props) {
           padding: "2%",
         }}
       >
-        <iframe
-          height="500px"
-          src="https://nusmods.com/timetable/sem-2/share?CS2105=TUT:05,LEC:1&GEH1049=LEC:3&IS4243=LEC:1&IS4261=LEC:1&ST2334=LEC:1,TUT:14"
-        ></iframe>
-
-        <Button variant="contained" style={{ width: "fit-content" }}>
-          Default
+        Student Id ({studentId}) has these events: <br></br>
+        {JSON.stringify(studentEvents)}
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ width: "fit-content" }}
+          onClick={addEvent}
+        >
+          Add Event
         </Button>
         <Button
           variant="contained"
           color="primary"
           style={{ width: "fit-content" }}
+          onClick={addStudent}
         >
-          Primary
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          style={{ width: "fit-content" }}
-        >
-          Secondary
-        </Button>
-        <Button variant="contained" style={{ width: "fit-content" }} disabled>
-          Disabled
+          Add Student
         </Button>
       </div>
 
-      <Body getUsers={getUsers} userList={userList} />
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          wigth: "100%",
+        }}
+      >
+        <div
+          style={{
+            marginTop: 50,
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={() => {
+              if (getStudents() !== undefined) {
+                getStudents();
+              } else console.log("lmao it didnt work");
+            }}
+          >
+            Get ALL student information from firebase
+          </Button>
+        </div>
+        <div>
+          {studentsList && studentsList.length > 0 ? (
+            studentsList.map((val) => {
+              return (
+                <div>
+                  {JSON.stringify(val)} <br></br>
+                </div>
+              );
+            })
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
     </Fragment>
   );
 }
