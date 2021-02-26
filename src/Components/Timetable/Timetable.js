f9eb90b4b6cbfd68481bee039f0cbf1b157d543e
handshou <hanselhub@gmail.com>
2021-02-27 00:07:57 +0800

    Clean up code

--- src/Components/Timetable/Timetable.js
+++ src/Components/Timetable/Timetable.js
@@ -3,41 +3,38 @@ import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 import './timetable.css';
-
 import {mock} from './mock';
 
-function createDays(name, modules) {
+function createDay(name, modules) {
   return {name, modules};
 }
 
-function createModule({title, date}) {
+function createModule(data) {
+  console.log({data});
+  const {title, type} = data;
   return (
     <>
       <div>{title}</div>
-      <div>{date}</div>
+      <div>{type}</div>
     </>
   );
 }
 
-function generateIntervals(startTime, endTime, minutesInterval) {
+function generateRows(startTime, endTime, minutesInterval) {
   // startTime = "0800";
   // endTime = "2330";
-  const result = [];
-
+  let result = [];
+  let minutes = '';
   while (parseInt(startTime) < parseInt(endTime)) {
     result.push(startTime);
-
     startTime = parseInt(startTime) + minutesInterval;
-
-    const minutes = String(startTime).slice(-2);
+    minutes = String(startTime).slice(-2);
     // console.log({startTime, minutes});
     if (parseInt(minutes) >= 60) {
       startTime = parseInt(startTime) - 60 + 100 + (minutes % 60);
     }
-
     startTime = String(startTime).padStart(4, '0');
   }
-
   // ["0800", "0830", "0900", ..., "2330"]
   return result;
 }
@@ -49,7 +46,6 @@ function makeDate(date) {
 function getModules() {
   const baseDate = new Date('Jan 11 2021 0:00 GMT+8').getTime();
   const weekInMilliSeconds = 1000 * 3600 * 24 * 7;
-
   const weeklyData = mock.map(d => ({
     week: Math.floor((d.startTime - baseDate) / weekInMilliSeconds) + 1,
     title: d.name,
@@ -60,21 +56,36 @@ function getModules() {
     endTime: String(makeDate(d.endTime).getHours())
       .padStart(2, '0')
       .concat(String(makeDate(d.endTime).getMinutes()).padEnd(2, '0')),
-    day: makeDate(d.startTime).toLocaleString('en-us', {weekday:'long'}),
+    day: makeDate(d.startTime).toLocaleString('en-us', {weekday: 'long'}),
   }));
-
   return weeklyData;
 }
 
-const mondayModules =  getModules().filter(m => m.week === 1).filter(m => m.day === 'Monday');
-
+const mondayModules = getModules()
+  .filter(m => m.week === 1)
+  .filter(m => m.day === 'Monday');
+const tuesdayModules = getModules()
+  .filter(m => m.week === 1)
+  .filter(m => m.day === 'Tuesday');
+const wednesdayModules = getModules()
+  .filter(m => m.week === 1)
+  .filter(m => m.day === 'Wednesday');
+const thursdayModules = getModules()
+  .filter(m => m.week === 1)
+  .filter(m => m.day === 'Thursday');
+const fridayModules = getModules()
+  .filter(m => m.week === 1)
+  .filter(m => m.day === 'Friday');
 const days = [
-    createDays('Monday', mondayModules),
+  createDay('Monday', mondayModules),
+  createDay('Tuesday', tuesdayModules),
+  createDay('Wednesday', wednesdayModules),
+  createDay('Thursday', thursdayModules),
+  createDay('Friday', fridayModules),
 ];
 
 export default function Timetable() {
-  const times = generateIntervals('0800', '2330', 30);
-
+  const times = generateRows('0800', '2330', 30);
   return (
     <div>
       <input accept="image/*" id="contained-button-file" multiple type="file" />
@@ -101,9 +112,7 @@ export default function Timetable() {
           <tbody>
             {days.map((day, i) => (
               <tr key={i}>
-                <td>
-                  {day.name}
-                </td>
+                <td>{day.name}</td>
                 {day.modules.map((module, j) => (
                   <td key={j} align="center">
                     {createModule(module)}
