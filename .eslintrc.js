ed119746d4ab67d01e9d194dfa3b42bf88db1869
handshou <hanselhub@gmail.com>
2021-02-28 02:30:08 +0800

    Use double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: ['react-app'],
+  extends: ["react-app"],
   rules: {},
   overrides: [
     {
-      files: ['**/*.js?(x)'],
+      files: ["**/*.js?(x)"],
       rules: {
-        eqeqeq: 'off',
-        'no-unused-vars': 'off',
-        'no-redeclare': 'off',
-        'no-loop-func': 'off',
-        'no-redeclare': 'off',
-        'react-hooks/exhaustive-deps': 'off',
+        eqeqeq: "off",
+        "no-unused-vars": "off",
+        "no-redeclare": "off",
+        "no-loop-func": "off",
+        "no-redeclare": "off",
+        "react-hooks/exhaustive-deps": "off",
       },
     },
   ],
