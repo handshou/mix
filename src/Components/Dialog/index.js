ef4e2d305dbdab9dc3e6725334318d20a7677f26
Hansel <hanselhub@gmail.com>
2021-04-10 15:38:12 +0800

    Correct Dialog error

--- src/Components/Dialog/index.js
+++ src/Components/Dialog/index.js
@@ -6,7 +6,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-export default function Dialog(props) {
+export default function GenericDialog(props) {
   const [open, setOpen] = React.useState(true);
   const { title, children, buttons } = props;
 
