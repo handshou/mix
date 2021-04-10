28ef4434d5cedba7906ce954424241e526f05895
Hansel <hanselhub@gmail.com>
2021-04-10 16:47:13 +0800

    Use HashRouter

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import firebase from "firebase";
 
 import MyModulesContext from "../Contexts/MyModulesContext";
